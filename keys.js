7884953738dceaf6bde13d13762d82616640a496
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-07-25 12:03:44 -0400

    creating switch function

--- keys.js
+++ keys.js
@@ -1,6 +1,9 @@
 console.log('this is loaded');
 
+var spotify = new Spotify(keys.spotify);
+
+
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.168cc78e772047c88bc4e6d4b96e13c1,
+  secret: process.env.4c1226265cf14f5c81c47274e3f45076
 };
--- liri.js
+++ liri.js
@@ -1,4 +1,28 @@
 var dontenv = require("dotenv").config();
-
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+var concert = process.argv[2];
+
+switch (concert) {
+    case `concert-this`:
+        total();
+        break;
+
+    case 'spotify-this-song':
+        total();
+        break;
+
+    case `movie-this`:
+        total();
+        break;
+
+    case `do-what-it-says`:
+        total();
+        break;
+}
+
+function total() {
+    
+}
+
