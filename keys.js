381c3ee37bbb1d76f8869b637d34aac6727ed06a
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-07-25 15:08:17 -0400

    built switch cases

--- keys.js
+++ keys.js
@@ -1,9 +1,6 @@
 console.log('this is loaded');
 
-var spotify = new Spotify(keys.spotify);
-
-
 exports.spotify = {
-  id: process.env.168cc78e772047c88bc4e6d4b96e13c1,
-  secret: process.env.4c1226265cf14f5c81c47274e3f45076
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,28 +1,46 @@
-var dontenv = require("dotenv").config();
+require("dotenv").config();
+var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var axios = require('axios');
+var fs = require("fs");
+// var spotify = new Spotify(keys.spotify);
 
-var concert = process.argv[2];
-
-switch (concert) {
+var command = process.argv[2];
+var searchTerm = process.argv.slice(3).join(" ");
+console.log(command);
+console.log(searchTerm);
+switch (command) {
     case `concert-this`:
-        total();
-        break;
-
-    case 'spotify-this-song':
-        total();
+        displayConcert();
         break;
 
     case `movie-this`:
-        total();
+        displayMovie();
+        break;
+
+    case 'spotify-this-song':
+        displaySong();
         break;
 
     case `do-what-it-says`:
-        total();
+        displayCommand();
         break;
+
+        default:
+            console.log("Please Enter a valid search");
 }
 
-function total() {
-    
+function displayConcert() {
+    console.log(value);
+    // dotenv.readFile("random.txt", function (err, data) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    //     data = data.split(", ");
+
+    //     for (var i = 0; i < process.argv.length; i++) {
+
+    //     }
+    // })
 }
 
